cf11580b54a0e39b5f9a7e83b77edd4f50572e41
Pinaki Patra <pinaki.patra.pro@gmail.com>
2019-04-20 05:53:28 +0000

    Add Renderer

--- src/ey/dqml/remediationdisplay/controls/RemediationDisplayRenderer.js
+++ src/ey/dqml/remediationdisplay/controls/RemediationDisplayRenderer.js
@@ -6,18 +6,45 @@
 sap.ui.define([],
 	function () {
 		"use strict";
-		var ExampleRenderer = {};
-		ExampleRenderer.render = function (oRm, oControl) {
+		var RemediationDisplayRenderer = {};
+		RemediationDisplayRenderer.render = function (oRm, oControl) {
+
+			//Write Parent
+			oRm.write('<div');
+			oRm.writeControlData(oControl);
+			oRm.addClass("parentCell");
+			oRm.writeClasses();
+			oRm.write('style="background:' + oControl.getBgColor() + ';"');
+			oRm.write(">");
+
+			//Write selection Indicator
 			oRm.write("<div");
 			oRm.writeControlData(oControl);
-			oRm.addClass("sapRULTExample");
+			oRm.addClass("selectionIndicator");
+			if (oControl.getSelected()) {
+				oRm.addClass("selectionIndicatorVisible");
+			}
 			oRm.writeClasses();
-			oRm.write(">");
-			oRm.write(sap.ui.getCore().getLibraryResourceBundle("ey.dqml.remediationdisplay").getText("ANY_TEXT"));
+			oRm.write(">&#10004</div>");
+			//Write detection area
+			if (oControl.getShowDetection()) {
+				oRm.write(
+					'<div class="detectionArea"> <span class="detectionArea">Indias Origin</span><span class="correctionIndicator error">  &#10006;</span> </div>'
+				);
+			}
+			if (oControl.getShowRemediation()) {
+				oRm.write(
+					'<div class="remediationArea"><span class="detectionArea">Indias Origin</span><span class="correctionIndicator success">  &#10004;</span></div>'
+				);
+				oRm.write(
+					' <div class="progressBar" width="1"><div class="progressValue">70%</div><div class="progressBarFilledContent"></div></div>');
+			}
+			//Write Tooltop
+			if (oControl.getTooltipText().length>0) {
+				oRm.write(' <span class="tooltiptext">' + oControl.getTooltipText() + '</span>');
+			}
 
-			oRm.writeEscaped(oControl.getText());
 			oRm.write("</div>");
 		};
-		return ExampleRenderer;
-
+		return RemediationDisplayRenderer;
 	},  true);
